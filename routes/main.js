05d2ffa2c1b0c3caf8c1254bbfb23f397813d108
pavitra-997 <100881473+pavitra-997@users.noreply.github.com>
2023-01-05 00:24:16 -0500

    initial commit

---
+++ routes/main.js
@@ -0,0 +1,14 @@
+const express= require('express')
+const router= express.Router()
+
+const authMiddleware = require('../middleware/auth')
+
+const{login,dashboard}=require('../controllers/main')
+
+
+router.route('/dashboard').get(authMiddleware, dashboard)
+router.route('/login').post(login)
+//router.route('/dashboard').get(dashboard)
+
+
+module.exports=router
